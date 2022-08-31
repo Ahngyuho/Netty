b8a2e593e55e01ffad49d021c4ec1dfc4e20da26
lab_502 <gyuho0897@gmail.com>
2022-08-31 21:18:45 +0900

    disconnect 후 다시 connect 하려고 했는데 Sharable 해야한다는 오류 발생 @Sharable 추가

--- src/main/java/com/netty_projext/discard/EchoServerHandler.java
+++ src/main/java/com/netty_projext/discard/EchoServerHandler.java
@@ -1,12 +1,16 @@
 package com.netty_projext.discard;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.nio.charset.Charset;
 
+import static io.netty.channel.ChannelHandler.*;
+
+//@Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
